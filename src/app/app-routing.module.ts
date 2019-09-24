6b9b9964fb6e11be07606a10cfb7c5fd72ceb6de
Andy <divine.moment.of.truth1@gmail.com>
2019-09-24 20:59:16 +0100

    initial commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,34 @@
+import { SignupComponent } from './auth/signup/signup.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { CelebrationsComponent } from './celebrations/celebrations.component';
+import { AccomodationComponent } from './accomodation/accomodation.component';
+import { FoodAndBeverageComponent } from './food-and-beverage/food-and-beverage.component';
+import { LifestyleComponent } from './lifestyle/lifestyle.component';
+import { WeddingsComponent } from './weddings/weddings.component';
+import { OffersComponent } from './offers/offers.component';
+
+const routes: Routes = [
+  { path: 'accomodation', component: AccomodationComponent },
+  { path: 'celebrations', component: CelebrationsComponent },
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+  { path: 'lifestyle', component: LifestyleComponent },
+  { path: 'food-and-beverage', component: FoodAndBeverageComponent },
+  { path: 'weddings', component: WeddingsComponent },
+  { path: 'offers', component: OffersComponent },
+  { path: 'signin', component: SigninComponent },
+  { path: 'signup', component: SignupComponent },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+
+export class AppRoutingModule {}
